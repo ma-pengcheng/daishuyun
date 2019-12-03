e2337dbb4738f62e6ab5338db98fc674453d4cef
mapengcheng <ma_pengcheng94@163.com>
2019-12-03 08:08:42 +0800

    数据可视化1.0.1

--- public/js/visualization.js
+++ public/js/visualization.js
@@ -13,4 +13,31 @@ window.onload = function(){
             videoDe.style.display = "block";  
         }
     })
+
+    // nav栏的定位类型切换
+    // 获取元素
+    var dataNav = document.querySelector(".data_nav");
+    var header = document.querySelector(".header");
+    var dataNavGroup = document.querySelector(".data_nav_group");
+    var as = dataNav.querySelectorAll("a");
+    // 注册事件
+    document.addEventListener("scroll",move)
+    function move(){
+        // console.log(window.pageYOffset);
+        if(window.pageYOffset >= 400){
+            dataNav.className = "data_nav data_nav_f";
+            header.className = "header header_f"
+        }else {
+            dataNav.className = "data_nav";
+            header.className = "header";
+        }
+    }
+    for(var i = 0;i < as.length;i++){
+        as[i].addEventListener("click",function(){
+            for(var i = 0;i < as.length;i++){
+                as[i].className = "";
+            }
+            this.className = "current";
+        })
+    }
 }
